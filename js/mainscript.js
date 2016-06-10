6b7f2de2038e6315d8733cd9d7281951b6e2a4c8
Brent Heigold <brent@heigoldinvestments.com>
2016-06-09 23:41:15 -0700

    Changed back to ,200

--- js/mainscript.js
+++ js/mainscript.js
@@ -366,7 +366,7 @@ $("#copy_price_to_percentage").click(function(){
         $("#yestCloseText").val("");
         $("#entryPrice").val("-----"); 
         $("#entryPercentage").val("-----");
-        $("#amountSpending").val("1100");
+        $("#amountSpending").val("1200");
         $("#orderStub").val("-----------------------"); 
 
         $("#roundShares_50").checked = true; 
