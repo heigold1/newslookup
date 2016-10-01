d4ea214b7ee14deed7b448a8cbf345db6f01d0a8
Brent Heigold <brent@heigoldinvestments.com>
2016-09-30 18:06:52 -0700

    Fix yesterday close to grab from ETRADE API

--- yesterday_close.php
+++ yesterday_close.php
@@ -1,97 +1,97 @@
-<?php 
+<?php
+/**
+ * E*TRADE PHP SDK
+ *
+ * @package    	PHP-SDK
+ * @version		1.1
+ * @copyright  	Copyright (c) 2012 E*TRADE FINANCIAL Corp.
+ *
+ */
+/*require_once("config.php");
+require_once(dirname(__FILE__) . '/../Common/Common.php');
+require_once 'Market/MarketClient.class.php';  */
 
 
-function get_friday_trade_date()
-{
-    $friday_trade_date = date('Y-m-d', strtotime("-75 hours"));
-    return $friday_trade_date;
-}
+//require_once("config.php");
+require_once(dirname(__FILE__) . '/Samples/Config.php');
+require_once(dirname(__FILE__) . '/Common/Common.php');
+require_once(dirname(__FILE__) . '/OAuth/etOAuth.class.php');
+require_once(dirname(__FILE__) . '/Market/MarketClient.class.php');
 
-function get_yesterday_trade_date()
-{
-    $yesterday_trade_date = date('Y-m-d', strtotime("-27 hours"));
-    return $yesterday_trade_date;
-}
+$symbol=$_GET['symbol'];
 
+$ini_array = parse_ini_file('etrade.ini', true);
 
+//$consumer 	= new etOAuthConsumer(ETWS_APP_KEY,ETWS_APP_SECRET);
+$consumer = new etOAuthConsumer($ini_array['OAuth']['oauth_consumer_key'],$ini_array['OAuth']['consumer_secret']);
+ 
+$consumer->oauth_token 			= $ini_array['OAuth']['oauth_token'];
+$consumer->oauth_token_secret 	= $ini_array['OAuth']['oauth_token_secret'];
 
-function grabHTML($url)
-{
+$ac_obj	= new MarketClient($consumer);
 
-$ch = curl_init();
-$header=array('GET /1575051 HTTP/1.1',
-    "Host: query.yahooapis.com",
-    'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language:en-US,en;q=0.8',
-    'Cache-Control:max-age=0',
-    'Connection:keep-alive',
-    'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36',
-    );
+	try{
+					
+		$request_params	= new getQuoteParams();
+		//		$request_params->__set('symbolList', array('GOOG','CSCO'));
+		$request_params->__set('symbolList', array($symbol));
+		$request_params->__set('detailFlag', 'All');
+		$out 	= $ac_obj->getQuote($request_params);
+	}catch(ETWSException $e){
+		echo 	"***Caught exception***  \n".
+				"Error Code 	: " . $e->getErrorCode()."\n" .
+				"Error Message 	: " . $e->getErrorMessage() . "\n" ;
+		if(DEBUG_MODE) echo $e->getTraceAsString() . "\n" ;
+		exit;
+	}catch(Exception $e){
+		echo 	"***Caught exception***  \n".
+				"Error Code 	: " . $e->getCode()."\n" .
+				"Error Message 	: " . $e->getMessage() . "\n" ;
+		if(DEBUG_MODE) echo $e->getTraceAsString() . "\n" ;
+		echo "Exiting...\n";
+		exit;
 
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 300);
-    curl_setopt( $ch, CURLOPT_COOKIESESSION, true );
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	}
 
-    curl_setopt($ch,CURLOPT_COOKIEFILE,'cookies.txt');
-    curl_setopt($ch,CURLOPT_COOKIEJAR,'cookies.txt');
-    curl_setopt($ch,CURLOPT_HTTPHEADER,$header);
+	$mkt_responce_obj = etHttpUtils::GetResponseObject($out);
 
-
-
-	curl_setopt($ch, CURLOPT_VERBOSE, true);
-	curl_setopt($ch, CURLOPT_STDERR,$f = fopen(__DIR__ . "/error.log", "w+"));
-	
-
-    $returnHTML = curl_exec($ch); 
-
-if($errno = curl_errno($ch)) {
-    $error_message = curl_strerror($errno);
-    echo "cURL error ({$errno}):\n {$error_message}";
-}   
-   curl_close($ch);
-    return $returnHTML; 
-} // end of function grabHTML
-
-    $current_time = date('Gis', strtotime("-3 hours")); 
-
-  	$symbol=$_GET['symbol'];
-  	$date_parameter = "";
+	$current_time = date('Gis', strtotime("-3 hours")); 
 
   	if ($current_time > 130000)  // 1:00 PM 
   	{
-  		// if it's after 1:00 (closing bell) then we go with today's date 
-  		$date_parameter = date('Y-m-d', strtotime("-3 hours"));
+  		if (isset($mkt_responce_obj->QuoteData->all->lastTrade))
+  		{
+  			$last_trade = (string) $mkt_responce_obj->QuoteData->all->lastTrade;
+
+  			if (preg_match('/E-4/', $last_trade))
+  			{
+  				$last_trade = floatval(preg_replace('/E-4/', '', $last_trade))/10000;
+
+  				echo $last_trade;
+  			}
+		}
+		else
+  		{
+			echo "------";
+		}
+
   	}
   	else
   	{
-  		// if it's still before 1:00 (closing bell) then we are using yesterday's date
-		$todays_date_weekday = date('l', strtotime("-3 hours"));   //   h:i:s A 
-    	if ($todays_date_weekday == "Monday")
-    	{
-      		$date_parameter = get_friday_trade_date();
-    	}
-    	else
-    	{
-	      	$date_parameter = get_yesterday_trade_date();
-    	}
+  		if (isset($mkt_responce_obj->QuoteData->all->prevClose))
+  		{
+  			$prev_close = (string) $mkt_responce_obj->QuoteData->all->prevClose;
+  			if (preg_match('/E-4/', $prev_close))
+  			{
+  				$prev_close = floatval(preg_replace('/E-4/', '', $prev_close))/10000;
 
+  				echo $prev_close;
+  			}
+		}
+		else
+		{
+			echo "------";
+		}
   	}
-
-	$fullURL = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20%20%20yahoo.finance.historicaldata%20where%20%20symbol%20%20%20%20=%20%22" . $symbol . "%22and%20%20%20%20startDate%20=%20%22" . $date_parameter . "%22%20and%20%20%20%20endDate%20%20%20=%20%22" . $date_parameter . "%22&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback="; 
-
-	$result = grabHTML($fullURL);
-
-	$result_json = json_decode($result);
-	if (isset($result_json->query->results))
-	{
-	  	echo $result_json->query->results->quote->Close;
-	}
-	else
-	{
-	  	echo "------";
-	}
-
 
 ?>
