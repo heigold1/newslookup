c6daa5e12a17ac58689982066951d4f4d0ebbde8
Brent Heigold <brent@heigoldinvestments.com>
2018-10-11 19:44:30 -0700

    Add daily VIX chart

--- js/mainscript.js
+++ js/mainscript.js
@@ -534,8 +534,8 @@ if ($.trim($("#quote_input").val()) != ""){
                 dataType: 'html',
                 success:  function (data) {
                   console.log(data);
-                  // this is for the symbol's 5-day chart
-                 $("div#bigcharts_yest_close").html(data + "<img style='max-width:100%; max-height:100%;' src='http://bigcharts.marketwatch.com/kaavio.Webhost/charts/big.chart?nosettings=1&symb=" + original_symbol + "&uf=0&type=2&size=2&freq=7&entitlementtoken=0c33378313484ba9b46b8e24ded87dd6&time=3&rand=" + Math.random() + "&compidx=&ma=0&maval=9&lf=1&lf2=0&lf3=0&height=335&width=579&mocktick=1'>"); 
+                  // the daily VIX, so you can see how the volatility goes throughout the day
+                 $("div#bigcharts_yest_close").html(data + "<img style='max-width:100%; max-height:100%;' src='http://bigcharts.marketwatch.com/kaavio.Webhost/charts/big.chart?nosettings=1&symb=VIX&uf=0&type=2&size=2&sid=1704273&style=320&freq=9&entitlementtoken=0c33378313484ba9b46b8e24ded87dd6&time=1&rand=" + Math.random() + "&compidx=&ma=0&maval=9&lf=1&lf2=0&lf3=0&height=335&width=579&mocktick=1'>");
                 }
             });  // end of AJAX call to bigcharts   
             $("div#bigcharts_chart_container").css("background-color", "#F3F3FF");                         
