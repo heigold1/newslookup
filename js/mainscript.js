3532bc36a43010ab58b545383d534f515fda9fa5
Brent Heigold <brent@heigoldinvestments.com>
2020-12-18 23:48:20 -0800

    News and changed purchase amout to ,500

--- js/mainscript.js
+++ js/mainscript.js
@@ -515,7 +515,7 @@ $(function() {
               $("#day1").html("");
               $("#entryPrice").val(""); 
               $("#entryPercentage").val("");  
-              $("#amountSpending").val("3000");
+              $("#amountSpending").val("3500");
               $("#eTradeLowPercentage").html("");
               $("#orderStub").val("-----------------------"); 
               $("#foreign_country").html("");
