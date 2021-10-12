49d8038463d48d6272c04d37819409c027168581
Brent Heigold <brent@heigoldinvestments.com>
2021-10-12 11:30:32 -0700

    Move StreetInsider frame around, resize

--- js/mainscript.js
+++ js/mainscript.js
@@ -856,7 +856,7 @@ This just gets the yesterday close and last vix values, we don't need these yet,
             (function(){
 
 /*               var eTradeIFrame = '<br><iframe id="etrade_iframe" src="https://www.etrade.wallst.com/v1/stocks/news/search_results.asp?symbol=' + symbol + '&rsO=new#lastTradeTime" width="675px" height="500px"></iframe>';  */
-               var streetInsiderIFrame = '<br><iframe src="https://www.streetinsider.com/stock_lookup.php?LookUp=Get+Quote&q=' + symbol + '#stock_pod_nav" width="675px" height="500px"></iframe>'; 
+               var streetInsiderIFrame = '<br><iframe src="https://www.streetinsider.com/stock_lookup.php?LookUp=Get+Quote&q=' + symbol + '#stock_pod_nav" width="675px" height="300px"></iframe>'; 
 
 
               $("div#left_bottom_container").css("background-color", "#BBDDFF");
@@ -905,7 +905,7 @@ This just gets the yesterday close and last vix values, we don't need these yet,
                         playDelist();
                       }
 
-                      $("div#left_bottom_container").html( data +  streetInsiderIFrame ); 
+                      $("div#left_bottom_container").html( streetInsiderIFrame + data); 
                   }
                 });  // end of AJAX call to marketwatch    
 
